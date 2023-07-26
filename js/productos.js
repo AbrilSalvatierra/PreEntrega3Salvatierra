4a51adb07660da3723669471fa22ad96b75acc88
Abril Salvatierra <salvatierraabril@gmail.com>
2023-07-25 23:09:33 -0300

    Preentrega3

---
+++ js/productos.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id:"1",
+        nombre:"Curry de Tomate",
+        images: "",
+        precio: 1300,
+        descripcion:"Curry hecho con base en tomate y guarnicion de arroz.",
+    },
+    {
+        id:"2",
+        nombre:"Milanesa con Pure",
+        images: "",
+        precio: 1500,
+        descripcion:"Milanesa de carne con guarnicion de pure de papas.",
+    },
+    {
+        id:"3",
+        nombre:"Rollos Primavera",
+        images: "",
+        precio: 1100,
+        descripcion:"Rollo elaborado de una pasta relleno de diferentes verduras picadas al estilo juliana.",
+    },
+    {
+        id:"4",
+        nombre:"Crispy Pork",
+        images: "",
+        precio: 1900,
+        descripcion:"Panceta crocante servida sobre arroz con huevo frito, pepino, pickles, chili oil y salsa de tamarindo.",
+    },
+    {
+        id:"5",
+        nombre:"Singaporean Noodles",
+        images: "",
+        precio: 1700,
+        descripcion:"Fideos vermicelli de arroz salteados con pollo y vegetales con salsa de soja y curry.",
+    },
+];
